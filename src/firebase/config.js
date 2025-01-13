ee52bda2144b461e89ea2e961ab3e4e28015d190
eloi <eloicram18@gmail.com>
2025-01-13 22:19:01 +0800

    FEAT: Added a firebase directory in the project

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCyeQ1bgvw7SVD-BbmdFsm6M9d5hRGz5JY",
+    authDomain: "vue-firebase-ea3fc.firebaseapp.com",
+    projectId: "vue-firebase-ea3fc",
+    storageBucket: "vue-firebase-ea3fc.firebasestorage.app",
+    messagingSenderId: "567690787885",
+    appId: "1:567690787885:web:4796e5c2b0f9edda864ca4"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+// Initialize Firestote service
+const projectFirestore = firebase.firestore()
+
+//Special firebase datatype
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
