516c6e53d6d5eabbe328a18fae03f0ab3780e888
eloi <eloicram18@gmail.com>
2025-01-13 22:37:11 +0800

    FEAT: Adds a component folder in the src directory to nest components in my project

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="email" v-model="email"/>
+    <input type="password" required placeholder="password" v-model="password"/>
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { auth } from '../firebase/config'
+
+export default {
+    setup() {
+        const email = ref('')
+        const password = ref('')
+
+        const handleSubmit = async () => {
+            console.log(email.value, password.value)
+            // try {
+            //     await auth.createUserWithEmailAndPassword(email.value, password.value)
+            //     const user = auth.currentUser
+            //     await user.updateProfile({
+            //         displayName: displayName.value
+            //     })
+            // } catch (error) {
+            //     console.error(error.message)
+            // }
+        }
+
+        return {
+            email,
+            password,
+            handleSubmit
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="display name" v-model="displayName"/>
+    <input type="email" required placeholder="email" v-model="email"/>
+    <input type="password" required placeholder="password" v-model="password"/>
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { auth } from '../firebase/config'
+
+export default {
+    setup() {
+        const displayName = ref('')
+        const email = ref('')
+        const password = ref('')
+
+        const handleSubmit = async () => {
+            console.log(displayName.value, email.value, password.value)
+            // try {
+            //     await auth.createUserWithEmailAndPassword(email.value, password.value)
+            //     const user = auth.currentUser
+            //     await user.updateProfile({
+            //         displayName: displayName.value
+            //     })
+            // } catch (error) {
+            //     console.error(error.message)
+            // }
+        }
+
+        return {
+            displayName,
+            email,
+            password,
+            handleSubmit
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
