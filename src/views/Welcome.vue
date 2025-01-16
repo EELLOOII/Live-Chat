8d1267656e195087457e9599d8bab311ee625764
eloi <eloicram18@gmail.com>
2025-01-16 22:31:01 +0800

    FEAT: Accepts the context.emit from SignupForm and LoginForm and use them for routing

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,14 +3,14 @@
     <p>Welcome</p>
     <div v-if="showLogin">
       <h2>Login</h2>
-      <LoginForm />
+      <LoginForm @login="enterChat"/>
       <p>Don't have an account yet?<span @click="showLogin = false"> Signup</span> instead</p>
       <!-- <button @click="showLogin = false">Sign up</button> -->
 
     </div>
     <div v-else>
       <h2>Sign up</h2>
-      <SignupForm />
+      <SignupForm @signup="enterChat"/>
      <p>Already Registered?<span @click="showLogin = true"> Login</span> instead</p>
     </div>
     <!-- <SignupForm /> -->
@@ -23,14 +23,21 @@
 import SignupForm from '../components/SignupForm.vue'
 import LoginForm  from '../components/LoginForm.vue'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default {
   components: { SignupForm, LoginForm },
   setup() {
     const showLogin = ref(true)
+    const router = useRouter()
+
+    const enterChat = () => {
+      router.push({ name: 'chatroom' })
+    }
 
     return {
-      showLogin
+      showLogin,
+      enterChat
     }
   }
 
