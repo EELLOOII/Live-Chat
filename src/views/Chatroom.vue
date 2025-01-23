37956332382158ac404cd3efc87ff9bee2bb5689
eloi <eloicram18@gmail.com>
2025-01-23 22:00:03 +0800

    FEAT: watch the current user, and if the current user is null, redirected to the login page

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,16 +1,34 @@
 <template>
-  <div class="container">
-    <Navbar />
-  </div>
+	<div class="container">
+		<Navbar />
+	</div>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue'
+import getUser from "@/composables/getUser"
+import Navbar from "@/components/Navbar.vue"
+import { watch } from "vue"
+import { useRouter } from "vue-router"
+
 export default {
-  components: { Navbar }
-}
+	components: { Navbar },
+	setup() {
+		const { user } = getUser()
+		const router = useRouter()
+
+		watch(user, () => {
+			if (!user.value) {
+        console.log("User is not logged in")				
+				router.push({ name: 'welcome' })
+			} else {
+        console.log("User is logged in")
+			}
+		});
+
+		return { user }
+	},
+};
 </script>
 
 <style>
-
 </style>
